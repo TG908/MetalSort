1ecdbae6e7ea68cfade30560bd6ab562eeb3a7ea
Tim Gymnich <TG908@users.noreply.github.com>
2017-10-10 00:15:15 +0200

    -implemented bitonic sorter

---
+++ MetalSort/Shader.metal
@@ -0,0 +1,31 @@
+//
+//  Shader.metal
+//  MetalSort
+//
+//  Created by Tim Gymnich on 13.09.17.
+//  Copyright © 2017 Tim Gymnich. All rights reserved.
+//
+
+#include <metal_stdlib>
+using namespace metal;
+
+typedef int DataType;
+
+
+kernel void parallelBitonic(device int *input [[buffer(0)]],
+                            constant int &p [[buffer(1)]],
+                            constant int &q [[buffer(2)]],
+                            uint gid [[thread_position_in_grid]])
+{
+    int distance = 1 << (p-q);
+    bool direction = ((gid >> p) & 2) == 0;
+    
+    if ((gid & distance) == 0 && (input[gid] > input[gid | distance]) == direction) {
+        DataType temp = input[gid];
+        input[gid] = input[gid | distance];
+        input[gid | distance] = temp;
+    }
+}
+
+
+
--- MetalSort/main.swift
+++ MetalSort/main.swift
@@ -6,7 +6,49 @@
 //  Copyright © 2017 Tim Gymnich. All rights reserved.
 //
 
-import Foundation
+import Metal
 
-print("Hello, World!")
+typealias DataType = CInt
 
+let device = MTLCreateSystemDefaultDevice()!
+let commandQueue = device.makeCommandQueue()!
+let library = device.makeDefaultLibrary()!
+let sortFunction = library.makeFunction(name: "parallelBitonic")!
+let pipeline = try! device.makeComputePipelineState(function: sortFunction)
+
+var data: [DataType] = [16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1]
+let dataBuffer = device.makeBuffer(bytes: &data, length: MemoryLayout<DataType>.stride * data.count, options: [.storageModeShared])!
+
+let threadgroupsPerGrid = MTLSize(width: data.count, height: 1, depth: 1)
+let threadsPerThreadgroup = MTLSize(width: pipeline.threadExecutionWidth, height: 1, depth: 1)
+
+guard let logn = Int(exactly: log2(Double(data.count))) else {
+    fatalError("data.count is not a power of 2")
+}
+
+for p in 0..<logn {
+    for q in 0..<p+1 {
+        
+        var n1 = p
+        var n2 = q
+        
+        let commands = commandQueue.makeCommandBuffer()!
+        let encoder = commands.makeComputeCommandEncoder()!
+            
+        encoder.setComputePipelineState(pipeline)
+        encoder.setBuffer(dataBuffer, offset: 0, index: 0)
+        encoder.setBytes(&n1, length: MemoryLayout<DataType>.stride, index: 1)
+        encoder.setBytes(&n2, length: MemoryLayout<DataType>.stride, index: 2)
+        encoder.dispatchThreadgroups(threadgroupsPerGrid, threadsPerThreadgroup: threadsPerThreadgroup)
+        encoder.endEncoding()
+            
+        commands.commit()
+        commands.waitUntilCompleted()
+    }
+}
+
+let dataPointer = dataBuffer.contents().assumingMemoryBound(to: DataType.self)
+let dataBufferPointer = UnsafeMutableBufferPointer(start: dataPointer, count: data.count)
+let resultsArray = Array.init(dataBufferPointer)
+
+print(resultsArray)
